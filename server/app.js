54672fdacf131b17a5b66274c52d3da00a2b9ad4
Tushar <tushar.nandy@vidyamantra.com>
2022-08-25 15:52:39 +0530

    connected to mongodb

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const mongoose = require("mongoose");
 
 const app = express()
 
+const url = "mongodb+srv://Duke_Blaze01:dragon123@cluster0.l389qd2.mongodb.net/test"
+
+mongoose.connect(url)
+mongoose.connection.once("open", () => {
+    console.log("connected to the databse successfully ")
+})
+
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
