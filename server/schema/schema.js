a299232b28562b7079e2f5da0d38b14f6ad0c35d
Tushar <tushar.nandy@vidyamantra.com>
2022-08-25 11:18:20 +0530

    Root Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
+// dummy data 
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
@@ -9,4 +15,22 @@ const BookType = new GraphQLObjectType({
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
     })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name: "RouteQeryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db / other sources
+            }
+        }
+    }
+})
+
+module.export = new GraphQLSchema({
+    query: RootQuery,
 })
